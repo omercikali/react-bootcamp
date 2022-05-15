36c50e42bacb340cb5f9fa7bc016122b4835085f
omercikali <63413613+omercikali@users.noreply.github.com>
2022-05-15 19:03:36 +0300

    hello

---
+++ src/Navic.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navic extends Component {
+  render() {
+    return (
+      <div>
+       <button> ben butonum</button>
+      </div>
+    )
+  }
+}
